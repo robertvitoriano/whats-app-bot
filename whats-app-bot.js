dabf528a5255c82fa9a4b8d932886c6af01d029e
robertvitoriano <robertvitorianodev@gmail.com>
2020-10-01 16:44:19 -0300

    begininning modularization

--- whats-app-bot.js
+++ whats-app-bot.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const { Client, Location, Chat, MessageMedia } = require("whatsapp-web.js");
+
+
 modelu.exports = {
+
   async createClient() {
     const SESSION_FILE_PATH = "./session.json";
     let sessionCfg;
-    let meny
     if (fs.existsSync(SESSION_FILE_PATH)) {
       sessionCfg = require(SESSION_FILE_PATH);
     }
@@ -16,6 +18,7 @@ modelu.exports = {
 
     client.initialize();
   },
+
   async sendAudio(url) {
     const buffer = fs.readFileSync(url);
     const encodedFile = buffer.toString("base64");
@@ -23,4 +26,5 @@ modelu.exports = {
     const music = new MessageMedia("audio/ogg", encodedFile, url);
     client.sendMessage(msg.from, music);
   },
+  
 };
